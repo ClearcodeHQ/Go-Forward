e9e0f0a4f9ee835872045b1180d2c6c8ee340a48
Łukasz Kostka <lukasz.kostka@netng.pl>
2016-08-12 20:03:54 +0200

    Move cloudwatch code to separate file

--- main.go
+++ main.go
@@ -3,34 +3,9 @@ package main
 import (
 	"fmt"
 	"net"
-	"time"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 )
 
 const listenAddress = "localhost:5514"
-
-// AWS CloudWatch specific constants.
-const (
-	// Maximum number of log events in a batch.
-	maxBatchEvents = 10000
-	// Maximum batch size in bytes.
-	maxBatchSize = 1048576
-	// A batch of log events in a single PutLogEvents request cannot span more than 24 hours.
-	maxBatchSpanTime = 24 * time.Hour
-	// How many bytes to append to each log event.
-	eventSizeOverhead = 26
-	// None of the log events in the batch can be more than 2 hours in the future.
-	eventFutureTimedelta = 2 * time.Hour
-	// None of the log events in the batch can be older than 14 days.
-	eventPastTimedelta = 14 * 24 * time.Hour
-	// DescribeLogStreams transactions/second.
-	describelogstreamsTps = 5
-)
-
-var params = &cloudwatchlogs.DescribeLogGroupsInput{Limit: aws.Int64(50)}
 
 func main() {
 	listenAddr, err := net.ResolveUDPAddr("udp", listenAddress)
@@ -42,6 +17,9 @@ func main() {
 		panic(err)
 	}
 	defer conn.Close()
+}
+
+func receiveEvents(conn *net.UDPConn, c chan syslogMessage) {
 	var buf [2048]byte
 	for {
 		n, err := conn.Read(buf[0:])
@@ -50,36 +28,9 @@ func main() {
 		}
 		parsed, err := decodeMessage(string(buf[0:n]))
 		if err == nil {
-			fmt.Println(parsed)
+			c <-parsed
 		} else {
 			fmt.Println(err)
 		}
 	}
-}
-
-func getLogGroups(region string) (groups []string) {
-	svc := cloudwatchlogs.New(session.New(), aws.NewConfig().WithRegion(region))
-
-	err := svc.DescribeLogGroupsPages(params,
-		func(page *cloudwatchlogs.DescribeLogGroupsOutput, lastPage bool) bool {
-			groups = append(groups, getGroupNames(page)...)
-			return !lastPage
-		})
-
-	if err != nil {
-		// Print the error, cast err to awserr.Error to get the Code and
-		// Message from an error.
-		// awsErr  := err.(awserr.Error)
-		// Generic AWS Error with Code, Message, and original error (if any)
-		// fmt.Println(awsErr.Code(), awsErr.Message())
-		panic(err)
-	}
-	return
-}
-
-func getGroupNames(page *cloudwatchlogs.DescribeLogGroupsOutput) (names []string) {
-	for _, val := range page.LogGroups {
-		names = append(names, *val.LogGroupName)
-	}
-	return
 }
