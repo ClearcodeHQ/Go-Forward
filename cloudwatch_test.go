b6d49cdaaa16296dfa2d093f03d59ff852a2fad2
Łukasz Kostka <lukasz.kostka@netng.pl>
2016-08-24 21:37:25 +0200

    Add timeSpan() test

--- cloudwatch_test.go
+++ cloudwatch_test.go
@@ -26,3 +26,20 @@ func TestMessageSorting(t *testing.T) {
 		}
 	}
 }
+
+func TestBatchTimeSpan(t *testing.T) {
+	oldest := time.Date(2016, 7, 23, 12, 48, 11, 969683000, time.UTC)
+	newest := time.Date(2016, 7, 23, 12, 48, 18, 969683000, time.UTC)
+	events := messageBatch{
+		logEvent{msg: &syslogMessage{timestamp: oldest}},
+		logEvent{msg: &syslogMessage{timestamp: time.Date(2016, 7, 23, 12, 48, 16, 969683000, time.UTC)}},
+		logEvent{msg: &syslogMessage{timestamp: newest}},
+	}
+
+	result := events.timeSpan()
+	expected := newest.Sub(oldest)
+
+	if result != expected {
+		t.Errorf("Time duration shoud be equal. Result: %v Expected: %v", result, expected)
+	}
+}
