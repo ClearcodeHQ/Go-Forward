eb792a1af6e82f9b79f289841fdc205a49f3ec8d
Łukasz Kostka <lukasz.kostka@netng.pl>
2016-10-20 00:13:42 +0200

    Added initial support for ini config file

---
+++ config.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/go-ini/ini"
+	"net/url"
+	"strings"
+)
+
+const generalSection = "general"
+
+type generalConfig struct {
+	role string
+}
+
+var (
+	errNameLooLong   = errors.New("name too long")
+	errInvalidName   = errors.New("invalid name")
+	errNameEmpty     = errors.New("empty name")
+	errInvalidScheme = errors.New("invalid network scheme")
+)
+
+// Read and return all sections from config file
+func getConfig(file string) (sections []*ini.Section, err error) {
+	config, err := ini.Load(file)
+	if err != nil {
+		return
+	}
+	sections = config.Sections()
+	return
+}
+
+// Read, validate and return all bons from sections
+// func getBonds(sections []*ini.Section) (bonds []streamBond, err error) {
+// 	for _, section := range sections {
+// 		if section.Name() != generalSection {
+//
+// 		}
+// 	}
+// }
+
+func validateSection(section *ini.Section) error {
+	var required = map[string]validateKeyFunc{
+		"group":  validateGroup,
+		"stream": validateStrean,
+		"source": validateSource,
+	}
+	// Check required keys
+	for key, keyfunc := range required {
+		if ok := section.HasKey(key); !ok {
+			return fmt.Errorf("missing key %s in section %s", key, section.Name())
+		}
+		// Validate values
+		if err := keyfunc(section.Key(key).String()); err != nil {
+			return fmt.Errorf("bad value of %s in section %s: %s", key, section.Name(), err)
+		}
+	}
+	return nil
+}
+
+type validateKeyFunc func(name string) error
+
+// Validate source URL
+func validateSource(name string) error {
+	uri, err := url.Parse(name)
+	if err != nil {
+		return err
+	}
+	// Valid schemes
+	var schemes = map[string]bool{
+		"udp": true,
+	}
+	// Check for valid scheme
+	if _, ok := schemes[uri.Scheme]; !ok {
+		return errInvalidScheme
+	}
+	return nil
+}
+
+/*
+http://docs.aws.amazon.com/AmazonCloudWatchLogs/latest/APIReference/API_CreateLogGroup.html
+Log group names can be between 1 and 512 characters long.
+Allowed characters are a-z, A-Z, 0-9, '_' (underscore), '-' (hyphen), '/' (forward slash), and '.' (period).
+*/
+func validateGroup(name string) error {
+	if name == "" {
+		return errNameEmpty
+	}
+	if len(name) > 512 {
+		return errNameLooLong
+	}
+	for _, char := range name {
+		if !strings.Contains("_-/.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", string(char)) {
+			return errInvalidName
+		}
+	}
+	return nil
+}
+
+/*
+http://docs.aws.amazon.com/AmazonCloudWatchLogs/latest/APIReference/API_CreateLogStream.html
+Log stream names can be between 1 and 512 characters long
+The ':' colon character is not allowed.
+*/
+func validateStrean(name string) error {
+	if name == "" {
+		return errNameEmpty
+	}
+	if len(name) > 512 {
+		return errNameLooLong
+	}
+	if strings.Contains(name, ":") {
+		return errInvalidName
+	}
+	return nil
+}
---
+++ config_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"github.com/go-ini/ini"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestValidateGroup_valid_chars(t *testing.T) {
+	err := validateGroup("_-/.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	assert.Nil(t, err)
+}
+
+func TestValidateGroup_too_long(t *testing.T) {
+	err := validateGroup(RandomString(518))
+	assert.Equal(t, err, errNameLooLong)
+}
+
+func TestValidateGroup_empty(t *testing.T) {
+	err := validateGroup("")
+	assert.NotNil(t, err)
+}
+
+func TestValidateGroup_invalid_strings(t *testing.T) {
+	for _, chr := range []string{",", "|", "ą"} {
+		err := validateGroup(chr)
+		assert.Equal(t, err, errInvalidName)
+	}
+}
+
+func TestValidateStream_too_long(t *testing.T) {
+	err := validateStrean(RandomString(518))
+	assert.Equal(t, err, errNameLooLong)
+}
+
+func TestValidateStream_empty(t *testing.T) {
+	err := validateStrean("")
+	assert.NotNil(t, err)
+}
+
+func TestValidateStream_invalid_strings(t *testing.T) {
+	err := validateGroup(":")
+	assert.Equal(t, err, errInvalidName)
+}
+
+func TestValidateSource_ok(t *testing.T) {
+	for _, uri := range []string{
+		"udp://localhost:5514",
+	} {
+		err := validateSource(uri)
+		assert.Nil(t, err)
+	}
+}
+
+func TestValidateSource_error(t *testing.T) {
+	for uri, expected := range map[string]error{
+		"tcp://localhost:5514": errInvalidScheme,
+	} {
+		err := validateSource(uri)
+		assert.Equal(t, err, expected)
+	}
+}
+
+func TestValidateSection_missing_key(t *testing.T) {
+	i, _ := ini.Load([]byte("[asd]"))
+	sec := i.Section("asd")
+	err := validateSection(sec)
+	assert.NotNil(t, err)
+}
+
+func TestValidateSection_ok(t *testing.T) {
+	i, _ := ini.Load([]byte("[asd]"))
+	sec := i.Section("asd")
+	for key, val := range map[string]string{
+		"group":  "some_group_name",
+		"stream": "stream_name",
+		"source": "udp://localhost:5514",
+	} {
+		sec.NewKey(key, val)
+	}
+	err := validateSection(sec)
+	assert.Nil(t, err)
+}
